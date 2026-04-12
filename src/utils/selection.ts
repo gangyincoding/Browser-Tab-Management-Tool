65f2987286bbcae2b3ca8aa573f8f206d1e5f7e7
gangy <SampleRound@163.com>
2026-04-12 20:24:28 +0800

    chore: reset repository to clean empty state

--- src/utils/selection.ts
+++
@@ -1,5 +0,0 @@
-import type { TabRecord } from "../types/tab";
-
-export function getUniqueVisibleTabIds(tabs: Pick<TabRecord, "id" | "isDuplicate">[]): string[] {
-  return tabs.filter((tab) => !tab.isDuplicate).map((tab) => tab.id);
-}
